663b471626d09a860fd0bd4fae955573a4b55cd7
mac <mac@Raymonds-MacBook-Pro.local>
2020-04-30 16:50:01 +0100

    Updates

---
+++ precache-manifest.55f2a796f830bf979c8f91d6205dd09c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c5961fd753f1f43263900d63fe6b2c8",
+    "url": "/E-commerce-App/index.html"
+  },
+  {
+    "revision": "88aeb84bd3b25dbee21e",
+    "url": "/E-commerce-App/static/css/main.f4f49a8d.chunk.css"
+  },
+  {
+    "revision": "00331c65711a60b4f8a0",
+    "url": "/E-commerce-App/static/js/2.f7338bee.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/E-commerce-App/static/js/2.f7338bee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88aeb84bd3b25dbee21e",
+    "url": "/E-commerce-App/static/js/main.43c23eb8.chunk.js"
+  },
+  {
+    "revision": "1356a0b9eb1ab01e2b51",
+    "url": "/E-commerce-App/static/js/runtime-main.1e553814.js"
+  }
+]);
